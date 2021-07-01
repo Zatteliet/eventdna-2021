29ba4ecd4531906a79994700740eb86dc70e3243
camiel <camiel.co@pixel-nexus.com>
2021-07-01 18:41:04 +0200

    Simplified code

--- experiments/prepare_data.py
+++ experiments/prepare_data.py
@@ -1,52 +1,50 @@
 import json
 from pathlib import Path
+from tempfile import TemporaryDirectory
 from zipfile import ZipFile
 
 from wasabi import msg
 
 from experiments.featurizer import featurize
 from experiments.iob_fmt import get_iob
-from experiments.util import clean_dir
 
 
-class DataPrepper:
+class Corpus:
     def __init__(self, corpus_path: Path) -> None:
-        self.corpus_path: Path = corpus_path
+        self.zip: Path = corpus_path
 
-        self.corpus = Path("./temp")
-        if not self.corpus.exists():
-            self.corpus.mkdir()
-        else:
-            msg.warn(
-                f"Corpus path at <{self.corpus}> already exists. Removing its contents."
-            )
+        msg.info(f"Extracting corpus...")
+        with TemporaryDirectory() as d:
+            with ZipFile(self.zip) as z:
+                z.extractall(d)
+                self.Xy = self._get_Xy(d)
 
-        # Read in and validate the corpus.
-        # Note that the data is not read in memory, but stored in a temporary directory.
-        msg.info(f"Extracting corpus to <{self.corpus}>...")
-        with ZipFile(corpus_path) as z:
-            z.extractall(self.corpus)
+    def write_Xy(self, out_path: Path) -> None:
+        with open(out_path, "w") as f:
+            json.dump(self.Xy, f, indent=4, sort_keys=True)
+        msg.good(f"Written Xy data at: {out_path}")
 
-    def write_Xy(self, out_path: Path, info_out_path: Path):
+    def _get_Xy(self, data_dir) -> list:
+        """Get data features and labels. Return these as an (X, y) tuple."""
 
         # Every sentence in the corpus will become a training example.
-        X = []
-        Y = []
+        # An example is a dict with three keys: X, y and doc_id.
+        Xy = []
 
         msg.info("Featurizing data...")
-        for doc_dir in self.corpus.iterdir():
+        for doc_dir in Path(data_dir).iterdir():
             doc_id = doc_dir.stem
 
             # Extract X features.
-            x: dict = featurize(
+            X_instance: dict = featurize(
                 dnaf=doc_dir / "dnaf.json",
                 lets=doc_dir / "lets.csv",
                 alpino=doc_dir / "alpino",
             )
-            x["id"] = doc_id
-            X.append(x)
 
             # Extract y labels (IOB sequences).
-            y: dict = get_iob(dnaf=doc_dir / "dnaf.json")
-            y["id"] = doc_id
-            Y.append(y)
+            y_instance: dict = get_iob(dnaf=doc_dir / "dnaf.json")
+
+            Xy.append({"X": X_instance, "y": y_instance, "doc_id": doc_id})
+
+        return Xy
--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-from experiments.prepare_data import DataPrepper
+from experiments.prepare_data import Corpus
 
 
 def test_dataprepper():
     case = Path("assets/eventdna_data/EventDNA_dnaf_corpus.zip")
     out_dir = Path("tests/data/out")
 
-    prepper = DataPrepper(case)
-    prepper.write_Xy(out_dir / "out.whatever", out_dir / "out.info")
+    prepper = Corpus(case)
+    prepper.write_Xy(out_dir/"Xy.json")
