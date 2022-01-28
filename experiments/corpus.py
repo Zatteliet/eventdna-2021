17b82f2fcff94d54d54ad4afc9f4b261cdfba709
camiel <camiel.co@pixel-nexus.com>
2022-01-28 11:25:41 +0100

    Adapted corpus reader to new data zip

--- experiments/corpus.py
+++ experiments/corpus.py
@@ -1,15 +1,15 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 from zipfile import ZipFile
-
 from loguru import logger
 
 from experiments.errors import FeaturizationError
 from experiments.featurizer import featurize
 from experiments.iob_fmt import get_iob
 
-
-DATA_ZIP = Path("data/eventdna_corpus_2020-13-02.zip")
+DATA_ZIPPED = Path("data/eventdna_corpus_2020-13-02.zip")
+DATA_EXTRACTED = DATA_ZIPPED.parent / "extracted"
 
 
 @dataclass
@@ -19,68 +19,61 @@ class Example:
     y: dict
 
 
-class Corpus:
-
-    def __init__(self, data_zip) -> None:
-
-        # Extract the corpus data to `data_dir` if it does not already exist.
-        self.data_dir = data_zip.parent / f"{data_zip}_extracted"
-        if not self.data_dir.exists():
-            self.data_dir.mkdir()
-            logger.info(f"Extracting corpus to {self.data_dir}")
-            with ZipFile(self.data_zip) as z:
-                z.extractall(self.data_dir)
-        else:
-            logger.info(
-                f"Found data dir: {self.data_dir}. Assuming it's ok to use it."
-            )
-
-    def examples(self) -> list:
-        """Read and yield features and labels from a data dir."""
-
-        # Every sentence in the corpus will become a training example.
-
-        # The data in data_dir is given in the form of documents.
-        # Transform these to the sentence level.
-        # TODO adapt logic to work on sentence level.
-
-        logger.info("Featurizing data...")
-        for doc_dir in Path(self.data_dir).iterdir():
-            try:
-                examples = list(self._examples_from_dir(doc_dir))
-            except FeaturizationError as e:
-                logger.error(e)
-            for example in examples:
-                yield example
-
-    @staticmethod
-    def _examples_from_dir(doc_dir):
-        """Stream examples from a single document directory."""
-
-        dnaf = doc_dir / "dnaf.json"
-        lets = doc_dir / "lets.csv"
-
-        # Extract X and y features.
-        x_sents = list(featurize(dnaf, lets))
-        y_sents = list(get_iob(dnaf, main_events_only=True))
-
-        for (x_id, x), (y_id, y) in zip(x_sents, y_sents):
-
-            # Check the correct sentences are matched.
-            if x_id != y_id:
-                raise FeaturizationError("Sentence ids do not match.")
-
-            # Check the n of tokens in each sentence is the same.
-            if not len(x) == len(y):
-                m = "{}: number of tokens in x and y don't match.\n{} != {}".format(
-                    doc_dir.stem,
-                    [d["token"] for d in x],
-                    y,
-                )
-                raise FeaturizationError(m)
-
-            ex_id = f"{doc_dir.stem}_{x_id}"
-            yield Example(id=ex_id, x=x, y=y)
+def get_examples(data_dir):
+    """Read and yield features and labels from a data dir.
+    Every sentence in the corpus will become a training example.
+    """
+    logger.info("Featurizing data...")
+    for doc_id, dnaf_p, lets_p, _ in read_files(data_dir):
+        try:
+            examples = list(get_featurized_sents(doc_id, dnaf_p, lets_p))
+        except FeaturizationError as e:
+            logger.error(e)
+        for example in examples:
+            yield example
 
 
-def 
+def get_featurized_sents(doc_id: str, dnaf: Path, lets: Path):
+    """Stream examples from a single document directory."""
+
+    # Extract X and y features.
+    x_sents = list(featurize(dnaf, lets))
+    y_sents = list(get_iob(dnaf, main_events_only=True))
+
+    for (x_id, x), (y_id, y) in zip(x_sents, y_sents):
+
+        # Check the correct sentences are matched.
+        if x_id != y_id:
+            raise FeaturizationError("Sentence ids do not match.")
+
+        # Check the n of tokens in each sentence is the same.
+        if not len(x) == len(y):
+            t = [d["token"] for d in x]
+            m = f"{doc_id}: number of tokens in x and y don't match.\n{t} != {y}"
+            raise FeaturizationError(m)
+
+        ex_id = f"{doc_id}_{x_id}"
+        yield Example(id=ex_id, x=x, y=y)
+
+
+def check_extract(zip: Path, target: Path):
+    """Extract `zip_p` to `target` if this has not been done already."""
+    if not target.exists():
+        target.mkdir()
+        logger.info(f"Extracting corpus to {target}")
+        with ZipFile(zip) as z:
+            z.extractall(target)
+    else:
+        logger.info(f"Using existing data dir: {target}")
+
+
+def read_files(data_dir: Path):
+
+    all_paths = data_dir.rglob("*")
+
+    files = defaultdict(dict)
+    for p in all_paths:
+        if p.parent.stem in {"dnaf", "lets", "alpino"}:
+            files[str(p.stem)][str(p.parent.stem)] = p
+    for id, paths in files.items():
+        yield id, paths["dnaf"], paths["lets"], paths["alpino"]
--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,15 +1,20 @@
-from experiments.corpus import Corpus
-from pathlib import Path
+from experiments.corpus import (
+    read_files,
+    get_examples,
+    check_extract,
+    DATA_EXTRACTED,
+    DATA_ZIPPED,
+)
 
-def test_dataprepper():
-    corpus_path = Path(
-        "/mnt/c/Users/camie/OneDrive/LRE paper/data/EventDNA_dnaf_corpus.zip"
-    )
-    # out_path = Path("tests/data/out") / "Xy.json"
 
-    corpus = Corpus(corpus_path)
-    examples = list(corpus.examples())
+def test_read_files():
+    check_extract(DATA_ZIPPED, DATA_EXTRACTED)
+    file_sets = read_files(DATA_EXTRACTED)
+    assert len(list(file_sets)) == 1773
+
+
+def test_featurized_examples():
+    examples = list(get_examples(DATA_EXTRACTED))
     assert len(examples) > 0
     print(len(examples))
-
-    # corpus.write_Xy(out_path)
+    print(examples[0])
